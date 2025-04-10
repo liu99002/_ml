2639509572c6e5cda21fcad865ccde667ce84eb0
liu99002 <liu99002@gmail.com>
2025-04-10 11:08:00 +0800

    新增作業10新增作業10

---
+++ hw/hw10/hw10.py
@@ -0,0 +1,34 @@
+from google import genai
+from google.genai import types
+from PIL import Image
+from io import BytesIO
+import os
+import sys
+import PIL.Image
+from dotenv import load_dotenv
+
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+
+client = genai.Client()
+
+contents = ('Hi, can you create a 3d rendered image of a pig '
+            'with wings and a top hat flying over a happy '
+            'futuristic scifi city with lots of greenery?')
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-exp-image-generation",
+    contents=contents,
+    config=types.GenerateContentConfig(
+      response_modalities=['Text', 'Image']
+    )
+)
+
+for part in response.candidates[0].content.parts:
+  if part.text is not None:
+    print(part.text)
+  elif part.inline_data is not None:
+    image = Image.open(BytesIO((part.inline_data.data)))
+    image.save('gemini-native-image.png')
+    image.show()
